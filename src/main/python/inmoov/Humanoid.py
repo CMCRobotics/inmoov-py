fd18bf9d7fb5511547ec25e74390a0370770932b
Brice Copy <brice.copy@gmail.com>
2016-08-18 13:21:05 +0200

    updates

--- src/main/python/inmoov/Humanoid.py
+++ src/main/python/inmoov/Humanoid.py
@@ -16,13 +16,16 @@ class Humanoid(object):
     DIR_TIGHTEN = 0x01
     DIR_LOOSEN = 0x02
     
-    LEFT_FINGER_THUMB  = 0x01
-    LEFT_FINGER_INDEX  = 0x02
-    LEFT_FINGER_MIDDLE = 0x03
-    LEFT_FINGER_RING   = 0x04
-    LEFT_FINGER_PINKY  = 0x05
+    LEFT_FINGER_THUMB  = [0x01]
+    LEFT_FINGER_INDEX  = [0x02]
+    LEFT_FINGER_MIDDLE = [0x03]
+    LEFT_FINGER_RING   = [0x04]
+    LEFT_FINGER_PINKY  = [0x05]
+    LEFT_WRIST_ROTATION = [0x06]
+   
+    LEFT_FINGERS = LEFT_FINGER_THUMB + LEFT_FINGER_INDEX + LEFT_FINGER_MIDDLE + LEFT_FINGER_RING + LEFT_FINGER_PINKY
     
-    LEFT_FINGERS = [ LEFT_FINGER_THUMB, LEFT_FINGER_INDEX, LEFT_FINGER_MIDDLE, LEFT_FINGER_RING, LEFT_FINGER_PINKY ]
+    ALL_SERVOS = LEFT_FINGERS + LEFT_WRIST_ROTATION
     
     
     
@@ -31,12 +34,13 @@ class Humanoid(object):
         pass
 
     @abstractmethod
-    def fingerTensionDelta(self, fingers, direction = DIR_LOOSEN, delta = 10, callback = None):
+    def finger_tension_delta(self, fingers, direction = DIR_LOOSEN, delta = 10, callback = None):
         pass
 
     @abstractmethod
     def stop(self, callback = None):
         pass
- 
 
-
+    @abstractmethod
+    def reset_all_servos(self, callback = None):
+        pass
--- src/main/python/inmoov/PwmHatHumanoid.py
+++ src/main/python/inmoov/PwmHatHumanoid.py
@@ -6,8 +6,10 @@ Created on Aug 04, 2016
 '''
 
 from inmmov.Humanoid import Humanoid
-from Adafruit_PWM_Servo_Driver import PWM
+import Adafruit_PCA9685
 import time
+import logging
+
 
 class PwmHatHumanoid(Humanoid):
     HKING_SERVO_MIN = 230  # Min pulse length out of 4096
@@ -19,15 +21,19 @@ class PwmHatHumanoid(Humanoid):
     BLACK_SERVO_MED = 120+265  # Max pulse length out of 4096
    
     def __init__(self):
+        self.logger = logging.getLogger(__name__)
         # Initialise the PWM device using the default address
-        self.pwm = PWM(0x40)
-        self.pwm.setPWMFreq(60)
+        self.pwm = Adafruit_PCA9685.PCA9685()
+        # Alternatively specify a different address and/or bus:
+        #self.pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
+        
+        self.pwm.set_pwm_freq(60)
         # this member array stores the PWM status of all servos.
         self.servoPWM = []
         
         # Set all servos to medium position
         for x in LEFT_FINGERS:
-            self._applyPWM(x,0, HKING_SERVO_MED)
+            self._apply_pwm(x,0, HKING_SERVO_MED)
 
         self.initialized = False
         
@@ -40,7 +46,7 @@ class PwmHatHumanoid(Humanoid):
     '''
       apply a delta to the given finger (loosen or tighten)
     '''
-    def fingerTensionDelta(self, fingers, direction = Humanoid.DIR_LOOSEN, delta = 10, callback = None):
+    def finger_tension_delta(self, fingers, direction = Humanoid.DIR_LOOSEN, delta = 10, callback = None):
         dir = (-1 if direction == Humanoid.DIR_LOOSEN else 1)
         for f in fingers:
             dlt = delta * dir
@@ -49,7 +55,7 @@ class PwmHatHumanoid(Humanoid):
                 newVal = max(HKING_SERVO_MIN, self.servoPWM[f] + dlt)
             else:
                 newVal = min(HKING_SERVO_MAX, self.servoPWM[f] + dit)
-            self._applyPWM(f, 0, newVal)
+            self._apply_pwm(f, 0, newVal)
             
         if callback is not None:
             callback()
@@ -62,10 +68,16 @@ class PwmHatHumanoid(Humanoid):
     '''
      Apply a new PWM and store the known value
     '''
-    def _applyPWM(self, servo, on=0, off=HKING_SERVO_MED):
-        self.pwm.setPWM(servo, on, off)
+    def _apply_pwm(self, servo, on=0, off=HKING_SERVO_MED):
+        self.pwm.set_pwm(servo, on, off)
         self.servoPWM[servo] = off
  
+    def reset_all_servos(self, callback = None):
+        for srvo in Humanoid.ALL_SERVOS:
+            self._apply_pwm(srvo,0,0)
+        if callbox is not None:
+            callback()
+        
 Humanoid.register(PwmHatHumanoid)
 
 if __name__ == '__main__':
